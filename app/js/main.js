e7618bd652184db3f52c73ef8568aefa7951b5a2
MEldorbek <eldorbekm94@gmail.com>
2021-10-06 19:54:52 +0500

    added sections

---
+++ app/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('.section-one__slider .slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        asNavFor: '.section-one__slider .slider__dots',
+        autoplay: true,
+    });
+    $('.section-one__slider .slider__dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        focusOnSelect: true,
+        asNavFor: '.section-one__slider .slider__inner',
+        dots: false,
+        arrows: false,
+    });
+    $('.section-two__slider .slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        asNavFor: '.section-two__slider .slider__dots',
+        autoplay: true,
+    });
+    $('.section-two__slider .slider__dots').slick({
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        focusOnSelect: true,
+        asNavFor: '.section-two__slider .slider__inner',
+        dots: false,
+        arrows: false,
+    });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ gulp.task('style', function() {
     return gulp.src([
             'node_modules/normalize.css/normalize.css',
             'node_modules/slick-carousel/slick/slick.css',
+            'node_modules/slick-carousel/slick/slick-theme.css',
         ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
